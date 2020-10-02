85c2ff83a86c6577d3e08f9d5ddb107bb191518f
bahar <baharkholdisabeti@gmail.com>
2020-10-02 01:00:46 -0400

    Search function added

--- foodbank_site/foodbank/urls.py
+++ foodbank_site/foodbank/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('search/', views.search_results, name='search_results'),
+    path('about/', views.about, name='about'),
 ]
--- foodbank_site/foodbank/views.py
+++ foodbank_site/foodbank/views.py
@@ -1,6 +1,7 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 from foodbank.models import Branch, BranchNeed
+from django.db.models import Q
 from .forms import SearchForm
 
 def index(request):
@@ -18,24 +19,22 @@ def index(request):
             this_needs.append(str(y))
         branches_needs.append(this_needs)
     
-    # now to make the search bar
+    # search by branch form
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = SearchForm(request.POST)
         # check whether it's valid:
-        #if form.is_valid():
+        if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
+
             # redirect to a new URL:
-            # return HttpResponseRedirect('/thanks/')
+            return render(request, 'search_results.html')
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = SearchForm()
-        
-    # return render(request, 'index.html', {'form': form})
-    
+
     context = {
         'num_branches': num_branches,
         'num_branch_needs': num_branch_needs,
@@ -45,3 +44,32 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+def search_results(request):
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data['branch_form']
+            results = []
+            for word in search.split(): #duplicates are allowed rn
+                y = Branch.objects.filter(Q(address__icontains=word) |  Q(city__icontains=word) | Q(province__icontains=word) | Q(postal_code__icontains=word))
+                for x in y:
+                    results.append(str(x))
+            # redirect to a new URL:
+            context = {
+                'query': search,
+                'results': results,
+            }
+            return render(request, 'search_results.html', context=context)
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = SearchForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'search_results.html', context = context)
+
+def about(request):
+    return render(request, 'about.html')
