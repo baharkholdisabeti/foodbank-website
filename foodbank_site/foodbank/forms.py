9914c7c74f60f0a7cf566bdbc97cb20265f9987c
bahar <baharkholdisabeti@gmail.com>
2020-10-24 21:27:17 -0400

    background image

--- foodbank_site/foodbank/forms.py
+++ foodbank_site/foodbank/forms.py
@@ -3,8 +3,12 @@ from django import forms
 class SearchForm(forms.Form):
     branch_form = forms.CharField(label='', max_length=100)
 
+categories = [
+        ("media/milk.png", "Milk"),
+        ("media/bread.jpg", "Bread"),
+    ]
+    
 class FilterForm(forms.Form):
-    categories = [zip("Milk", 'media/milk.png'), zip("Dairy", 'media/dairy.jpg')]
     needs = forms.MultipleChoiceField(
         required=False,
         widget=forms.CheckboxSelectMultiple,
--- foodbank_site/foodbank/views.py
+++ foodbank_site/foodbank/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from foodbank.models import Branch, BranchNeed
 from django.db.models import Q
 from .forms import SearchForm
+from .forms import FilterForm
 
 def index(request):
     branches_list = Branch.objects.all()
@@ -14,18 +15,25 @@ def index(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         form = SearchForm(request.POST)
-        if form.is_valid():
-            query = form.cleaned_data['branch_form']
-            branches_list = Branch.objects.all()
-            for branch in branches_list:
-                for word in query.split(): 
-                    if (branch.address.upper() in word.upper()) or (branch.city.upper() in word.upper()) or \
-                         (branch.province.upper() in word.upper()) or (branch.postal_code.upper() in word.upper()) or \
-                            (word.upper() in branch.address.upper()) or (word.upper() in branch.city.upper()) or \
-                                (word.upper() in branch.province.upper()) or (word.upper() in branch.postal_code.upper()):
-                        results.append(branch)
-                        break
-                continue
+        # check which form
+        if 'search' in request.POST:
+            if form.is_valid():
+                query = form.cleaned_data['branch_form']
+                branches_list = Branch.objects.all()
+                for branch in branches_list:
+                    for word in query.split(): 
+                        if (branch.address.upper() in word.upper()) or (branch.city.upper() in word.upper()) or \
+                            (branch.province.upper() in word.upper()) or (branch.postal_code.upper() in word.upper()) or \
+                                (word.upper() in branch.address.upper()) or (word.upper() in branch.city.upper()) or \
+                                    (word.upper() in branch.province.upper()) or (word.upper() in branch.postal_code.upper()):
+                            results.append(branch)
+                            break
+                    continue
+        else:
+            print("here")
+            if "milk" in request.POST:
+                print("HIIIII")
+
     # if a GET (or any other method) we'll create a blank form
     else:
         form = SearchForm()
