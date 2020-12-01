093ba2f302e35281ac6b85535cc89b0250167363
Bahar Kholdi-Sabeti <baharkholdisabeti@gmail.com>
2020-12-01 01:26:54 -0500

    edit allowed hosts

--- foodbank_site/settings.py
+++ foodbank_site/settings.py
@@ -14,7 +14,9 @@ SECRET_KEY = 'cvrcez@wj**3e1+a5b2b-d8rqu-7kf=jch%e2s!n-@hn!$h)4q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'protected-falls-29001.herokuapp.com'
+]
 
 
 # Application definition
