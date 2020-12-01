67568c76fbcd60fb5aeffb44f5dc1393b312bbbc
Bahar Kholdi-Sabeti <baharkholdisabeti@gmail.com>
2020-11-30 23:53:13 -0500

    added redirect, replaced venv w/ requirements.txt

--- foodbank_site/foodbank/urls.py
+++ foodbank_site/foodbank/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
+from django.views.generic import RedirectView
+import re
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    re_path(r'^$', RedirectView.as_view(url='foodbank/')),
     path('about/', views.about, name='about'),
+    path('foodbank/', views.index, name='index'),
 ]
