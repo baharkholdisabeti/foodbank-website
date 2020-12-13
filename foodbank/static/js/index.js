41e395fa0d2d283deda7fd21cffb1d940dd26049
Bahar Kholdi-Sabeti <baharkholdisabeti@gmail.com>
2020-12-13 13:37:06 -0500

    js image checkbox implemented

--- foodbank/static/js/index.js
+++ foodbank/static/js/index.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
-
+    $("img").imgCheckbox({
+        onload: function(){
+            // Do something fantastic!
+            $(this).select();
+        },
+        onclick: function(el){
+            var isChecked = el.hasClass("imgChked"),
+            imgEl = el.children()[0];  // the img element
+            
+        console.log(imgEl.name + " is now " + (isChecked? "checked": "not-checked") + "!");
+        }
+    });
 });
