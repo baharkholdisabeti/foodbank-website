3ac4a576655170a06166dad2c6b6851fa71b5948
Bahar Kholdi-Sabeti <baharkholdisabeti@gmail.com>
2021-01-13 05:46:53 -0500

    update static vars for deployment

--- foodbank_site/settings.py
+++ foodbank_site/settings.py
@@ -111,11 +111,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT= 'https://foodbank-shortages.herokuapp.com/foodbank/foodbank/static'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/foodbank/static/'
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'foodbank/static')
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'foodbank/static')
+]
 
 # get env variables (for API keys and such)
 GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
