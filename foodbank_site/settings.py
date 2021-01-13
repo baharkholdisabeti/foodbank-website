eca2b43c5b742346f09659ca4caabbb5f36ed8d9
Bahar Kholdi-Sabeti <baharkholdisabeti@gmail.com>
2021-01-13 06:10:42 -0500

    add gzip for quicker app

--- foodbank_site/settings.py
+++ foodbank_site/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.gzip',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
